18b74ba114e39d11064ac4f92ca2b52ad091667d
Corey337337 <egorvasilevic95@gmail.com>
2026-07-05 13:09:55 +0300

    add class Music and music collection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,45 @@ using System.Windows.Shapes;
 
 namespace to_do_list
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public int Year { get; set; }
+
+        public Music(string title, string artist, string album, int year)
+        {
+            Title = title;
+            Artist = artist;
+            Album = album;
+            Year = year;
+        }
+
+    }
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Music> allMusic;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            allMusic = new ObservableCollection<Music>
+            {
+                new Music("No Surprises", "Radiohead", "OK Computer", 1997),
+                new Music("Song 2", "Artist 2", "Album 2", 2021),
+                new Music("Song 3", "Artist 3", "Album 3", 2022),
+            };
+
+            MusicListView.ItemsSource = allMusic;
+        }
+
+        
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
